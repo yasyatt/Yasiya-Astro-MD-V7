38eb93b1ada82bb20ec41389e266dc1c9b0dcb04
Vihanga Ashinshana <96816258+vihangayt0@users.noreply.github.com>
2023-02-12 14:54:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ ᴀ
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? '-' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ZjlyUHg_astro_3TXY=' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID
 };
